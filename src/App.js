c936838df9230e695236d196a0c8d472d5dec082
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-10 20:53:22 +0300

    create EngineVolumeService

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import ModelsPage from "./components/pages/ModelsPage";
-import { Button, Space, DatePicker, version } from 'antd';
 import BrandsPage from "./components/pages/BrandsPage";
 import 'react-toastify/dist/ReactToastify.css';
-
-import {QR} from './components/qr'
 import {ToastContainer} from "react-toastify";
 
-
-export function App() {
+export default function App() {
     return (
         <>
-            <ToastContainer />
-            {/*<ModelsPage />*/}
-            <BrandsPage />
-            {/*<QR />*/}
+            <ToastContainer/>
+            <BrandsPage/>
         </>
     )
 }
 
-export default App;
