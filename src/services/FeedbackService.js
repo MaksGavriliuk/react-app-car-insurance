e033fbe705722aeb5f9e1aaae701c5b7a78152bf
MaksGavriliuk <maks.revival27@gmail.com>
2023-11-23 00:56:15 +0300

    create FeedbackService

---
+++ src/services/FeedbackService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+class FeedbackService {
+
+    async deleteFeedback(id) {
+        try {
+            const response = await axios.delete(`http://localhost:8080/feedbacks/${id}`);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async saveFeedback(feedback) {
+        try {
+            const response = await axios.post('http://localhost:8080/feedback/create', feedback);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
+
+export default new FeedbackService();
