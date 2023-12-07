7a29fe67d98c61e57cb6ed42cc024e6a8227342e
MaksGavriliuk <maks.revival27@gmail.com>
2023-12-07 14:54:47 +0300

    update contracts in UserProfile

--- src/components/Contracts/UserContractsTable.js
+++ src/components/Contracts/UserContractsTable.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Table, Empty, Button, Form, Input, Modal, Spin } from 'antd';
+import { Table, Empty, Spin } from 'antd';
 import contractService from "../../services/ContractServiceForUser";
-import { toast } from 'react-toastify';
+
 
 export default function UserContractsTable() {
 
@@ -29,19 +29,50 @@ export default function UserContractsTable() {
             key: 'id',
         },
         {
+            title: 'Марка',
+            dataIndex: ['userCar', 'car', 'model', 'brand', 'brand'],
+            key: 'brand',
+        },
+        {
+            title: 'Объём двигателя',
+            dataIndex: ['userCar', 'car', 'engineVolume', 'engineVolume'],
+            key: 'engineVolume',
+        },
+        {
+            title: 'Тип топлива',
+            dataIndex: ['userCar', 'car', 'fuelType', 'fuelType'],
+            key: 'fuelType',
+        },
+        {
+            title: 'ID страхователя',
+            dataIndex: ['insuranceAgent', 'id'],
+            key: 'insuranceAgentId',
+        },
+        {
             title: 'Тип страховки',
-            dataIndex: 'insuranceType',
+            dataIndex: ['insuranceType', 'insuranceType'],
             key: 'insuranceType',
         },
         {
             title: 'Дата начала',
             dataIndex: 'startDate',
             key: 'startDate',
+            render: (date) => {
+                return new Date(date).toLocaleDateString();
+            },
         },
         {
             title: 'Дата окончания',
             dataIndex: 'endDate',
             key: 'endDate',
+            render: (date) => {
+                return new Date(date).toLocaleDateString();
+            },
+        },
+        {
+            title: 'Тип страховки',
+            dataIndex: ['insuranceType', 'insuranceType'],
+            key: 'insuranceType',
         },
         {
             title: 'Стоимость',
@@ -52,7 +83,7 @@ export default function UserContractsTable() {
             title: 'Величина выплаты',
             dataIndex: 'payoutAmount',
             key: 'payoutAmount',
-        },{
+        }, {
             title: 'Статус',
             dataIndex: 'status',
             key: 'status',
