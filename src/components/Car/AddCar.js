c93f20e6eb9664210e3af28670bd79f6676904b6
MaksGavriliuk <maks.revival27@gmail.com>
2023-12-08 20:09:05 +0300

    update AddCar

--- src/components/Car/AddCar.js
+++ src/components/Car/AddCar.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Button, DatePicker, Form, InputNumber, message, Select } from 'antd';
+import React, {useEffect, useState} from 'react';
+import {Button, DatePicker, Form, InputNumber, message, Row, Select} from 'antd';
 import brandService from '../../services/BrandService';
 import modelService from '../../services/ModelService';
 import engineVolumeService from '../../services/EngineVolumeService';
@@ -8,7 +8,7 @@ import carService from '../../services/CarService';
 import Header from '../Header/Header';
 import './AddCar.css';
 
-const { Option } = Select;
+const {Option} = Select;
 
 export default function AddCar() {
     const [brands, setBrands] = useState([]);
@@ -59,12 +59,12 @@ export default function AddCar() {
             message.success('Машина успешно добавлена');
             form.resetFields();
         } catch (error) {
-            message.error('Ошибка при создании машины');
+            message.error('Ошибка при добавлении машины');
         }
     };
 
     const handleYearChange = (date) => {
-        form.setFieldsValue({ selectedYear: date });
+        form.setFieldsValue({selectedYear: date});
     };
 
     const disabledDate = (current) => {
@@ -74,82 +74,95 @@ export default function AddCar() {
 
     const handleCalculate = () => {
         // Логика расчета подробной информации о машине
-        setSelectedCar({ /* Данные о машине */ });
+        setSelectedCar({ /* Данные о машине */});
     };
 
     return (
         <>
-            <Header />
-            <div className="car-modal" style={{ width: '600px' }}>
+            <Header/>
+            <div className="car-modal" style={{width: '60%'}}>
                 <h2>Добавление автомобиля</h2>
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
                     <Form form={form} onFinish={handleAddCar}>
-                        <Form.Item name="selectedBrand" rules={[{ required: true, message: 'Введите марку' }]}>
-                            <Select placeholder="Выберите марку" style={{ width: '200px' }} onChange={handleBrandChange}>
-                                {brands &&
-                                    brands.map((brand) => (
-                                        <Option key={brand.id} value={brand.brand}>
-                                            {brand.brand}
-                                        </Option>
-                                    ))}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item name="selectedModel" rules={[{ required: true, message: 'Введите модель' }]}>
-                            <Select
-                                placeholder="Выберите модель"
-                                style={{ width: '200px' }}
-                                disabled={!form.getFieldValue('selectedBrand')}
-                            >
-                                {models.length > 0 &&
-                                    models.map((model) => (
-                                        <Option key={model.id} value={model.id}>
-                                            {model.model}
-                                        </Option>
-                                    ))}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item name="selectedEngineVolume" rules={[{ required: true, message: 'Введите объём двигателя' }]}>
-                            <Select placeholder="Выберите объём двигателя" style={{ width: '200px' }}>
-                                {engineVolumes &&
-                                    engineVolumes.map((engineVolume) => (
-                                        <Option key={engineVolume.id} value={engineVolume.id}>
-                                            {engineVolume.engineVolume}
-                                        </Option>
-                                    ))}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item name="selectedFuelType" rules={[{ required: true, message: 'Введите тип топлива' }]}>
-                            <Select placeholder="Выберите тип топлива" style={{ width: '200px' }}>
-                                {fuelTypes &&
-                                    fuelTypes.map((fuelType) => (
-                                        <Option key={fuelType.id} value={fuelType.id}>
-                                            {fuelType.fuelType}
-                                        </Option>
-                                    ))}
-                            </Select>
-                        </Form.Item>
-                        <Form.Item name="selectedYear" rules={[{ required: true, message: 'Введите год выпуска' }]}>
-                            <DatePicker
-                                placeholder="Выберите год выпуска"
-                                picker="year"
-                                format="YYYY"
-                                disabledDate={disabledDate}
-                                onChange={handleYearChange}
-                                style={{ width: '200px' }}
-                            />
-                        </Form.Item>
-                        <Form.Item name="selectedCurrentValue" rules={[{ required: true, message: 'Введите текущую стоимость' }]}>
-                            <InputNumber placeholder="Текущая стоимость" style={{ width: '200px' }} />
-                        </Form.Item>
-                        <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
-                            <Button type="primary" htmlType="submit" style={{ background: '#1890ff', borderColor: '#1890ff' }}>
-                                Рассчитать
+                        <Row>
+                            <div className={'input-container'}>
+                                <Form.Item name="selectedBrand" rules={[{required: true, message: 'Введите марку'}]}>
+                                    <Select placeholder="Выберите марку" style={{width: '200px'}}
+                                            onChange={handleBrandChange}>
+                                        {brands &&
+                                            brands.map((brand) => (
+                                                <Option key={brand.id} value={brand.brand}>
+                                                    {brand.brand}
+                                                </Option>
+                                            ))}
+                                    </Select>
+                                </Form.Item>
+                                <Form.Item name="selectedModel" rules={[{required: true, message: 'Введите модель'}]}>
+                                    <Select
+                                        placeholder="Выберите модель"
+                                        style={{width: '200px'}}
+                                        disabled={!form.getFieldValue('selectedBrand')}
+                                    >
+                                        {models.length > 0 &&
+                                            models.map((model) => (
+                                                <Option key={model.id} value={model.id}>
+                                                    {model.model}
+                                                </Option>
+                                            ))}
+                                    </Select>
+                                </Form.Item>
+                                <Form.Item name="selectedEngineVolume"
+                                           rules={[{required: true, message: 'Введите объём двигателя'}]}>
+                                    <Select placeholder="Выберите объём двигателя" style={{width: '200px'}}>
+                                        {engineVolumes &&
+                                            engineVolumes.map((engineVolume) => (
+                                                <Option key={engineVolume.id} value={engineVolume.id}>
+                                                    {engineVolume.engineVolume}
+                                                </Option>
+                                            ))}
+                                    </Select>
+                                </Form.Item>
+                            </div>
+                        </Row>
+                        <Row>
+                            <div className={'input-container'}>
+                                <Form.Item name="selectedFuelType"
+                                           rules={[{required: true, message: 'Введите тип топлива'}]}>
+                                    <Select placeholder="Выберите тип топлива" style={{width: '200px'}}>
+                                        {fuelTypes &&
+                                            fuelTypes.map((fuelType) => (
+                                                <Option key={fuelType.id} value={fuelType.id}>
+                                                    {fuelType.fuelType}
+                                                </Option>
+                                            ))}
+                                    </Select>
+                                </Form.Item>
+                                <Form.Item name="selectedYear"
+                                           rules={[{required: true, message: 'Введите год выпуска'}]}>
+                                    <DatePicker
+                                        placeholder="Выберите год выпуска"
+                                        picker="year"
+                                        format="YYYY"
+                                        disabledDate={disabledDate}
+                                        onChange={handleYearChange}
+                                        style={{width: '200px'}}
+                                    />
+                                </Form.Item>
+                                <Form.Item name="selectedCurrentValue"
+                                           rules={[{required: true, message: 'Введите текущую стоимость'}]}>
+                                    <InputNumber placeholder="Текущая стоимость" style={{width: '200px'}}/>
+                                </Form.Item>
+                            </div>
+                        </Row>
+                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                            <Button className="add-car-button" type="primary" htmlType="submit">
+                                Добавить
                             </Button>
                         </div>
                     </Form>
                 </div>
                 {selectedCar && (
-                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
                         <div className="car-details">{/* Подробная информация о машине */}</div>
                     </div>
                 )}
