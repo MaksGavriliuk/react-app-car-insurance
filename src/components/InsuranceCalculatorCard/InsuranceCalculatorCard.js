d7c4c21762e3c6a309eb3ba8ab6b3c5bfb011a27
MaksGavriliuk <maks.revival27@gmail.com>
2023-12-08 20:37:40 +0300

    update InsuranceCalculatorCard

--- src/components/InsuranceCalculatorCard/InsuranceCalculatorCard.js
+++ src/components/InsuranceCalculatorCard/InsuranceCalculatorCard.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import {Card, Typography, Image, Row, Button} from 'antd';
-import {Link, useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 import image from './osago.png';
-import './InsuranceCalculatorCard.css'; // Подключаем внешний CSS-файл для стилей
+import './InsuranceCalculatorCard.css';
 
 const {Title, Text} = Typography;
 
 export default function InsuranceCalculatorCard() {
-
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     function moveToCalculator() {
-        navigate('/calculate')
+        navigate('/calculate');
     }
 
     return (
@@ -19,31 +18,27 @@ export default function InsuranceCalculatorCard() {
             <Card hoverable className="card">
                 <div className="card-content">
                     <div className="text-content">
-                        <Title className="title">Расчёт стоимости страховки</Title>
+                        <Title className="title text-row">Оформите страховку за 20 секунд</Title>
                         <Text>В нашем сервисе доступно несколько видов страхования:</Text>
-                        <Row>
-                            <Text>
-                                Обязательное страхование граждан...
-                                <Link to="/osgovts"> Подробнее </Link>
-                            </Text>
-                        </Row>
-                        <Row>
-                            <Text>
-                                Каско
-                                <Link to="/casko"> Подробнее</Link>
-                            </Text>
-                        </Row>
-                        <Row>
-                            <Text>
-                                Зеленая карта
-                                <Link to="/green-card"> Подробнее</Link>
-                            </Text>
-                        </Row>
+                        <ul>
+                            <li>
+                                <Row className="text-row">
+                                    <Text>Обязательное страхование граждан</Text>
+                                </Row>
+                            </li>
+                            <li>
+                                <Row className="text-row">
+                                    <Text>Каско</Text>
+                                </Row>
+                            </li>
+                            <li>
+                                <Row className="text-row">
+                                    <Text>Зеленая карта</Text>
+                                </Row>
+                            </li>
+                        </ul>
 
-                        <Button onClick={moveToCalculator}>
-                            Перейти к расчёту
-                        </Button>
-
+                        <Button onClick={moveToCalculator}>Перейти к расчёту</Button>
                     </div>
                     <div className="image-content">
                         <Image src={image} preview={false}/>
